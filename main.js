577df6a635fc8e929e409f034e2d4f43eaa1e94d
KNXCSdev <michal.miksho@gmail.com>
2024-10-19 12:59:54 +0200

    Changed styling of the highest value

--- main.js
+++ main.js
@@ -5,14 +5,16 @@ const graph = document.querySelector(".main__graph");
 async function loadGraphData() {
   try {
     const response = await fetch("data.json");
-
     const arr = await response.json();
-    console.log(arr);
-    const maxAmount = Math.max(...arr.map((item) => item.amount));
 
+    let maxAmount = Math.max(...arr.map((item) => item.amount));
     const graphElements = document.querySelectorAll(".main__graph");
 
     // Iteration over the graph elements and setting their heights dynamically
+    graphElements.forEach((element) => {
+      element.classList.remove("main__graph--highest");
+    });
+
     graphElements.forEach((element, index) => {
       // Calculating the height as a percentage of the max amount
       const heightPercentage = (arr[index].amount / maxAmount) * 100;
@@ -22,8 +24,13 @@ async function loadGraphData() {
 
       //  Adding text showing the amount
       const valueElement = element.querySelector(".main__graph__value");
+
       if (valueElement) {
-        valueElement.textContent = `$${arr[index].amount.toFixed(2)}`;
+        valueElement.textContent = `${arr[index].amount.toFixed(2)}`;
+
+        if (arr[index].amount === maxAmount) {
+          element.classList.add("main__graph--highest");
+        }
       }
     });
   } catch (error) {
